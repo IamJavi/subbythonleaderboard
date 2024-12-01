16e951417c0e22c898c4e3beb17b83f4326440ff
Javier Mariscal <javi.mariscal2@gmail.com>
2024-12-01 11:34:59 -0500

    Updates

---
+++ precache-manifest.2c7cb1bd0caca615d7ac8c90b041fc1d.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7096554af66799e38fa45072e4520d2a",
+    "url": "/subbythonleaderboard/index.html"
+  },
+  {
+    "revision": "341cb40226484970b7db",
+    "url": "/subbythonleaderboard/static/css/main.7fe3de5c.chunk.css"
+  },
+  {
+    "revision": "6aeeb0ffe1dc776cf1fb",
+    "url": "/subbythonleaderboard/static/js/2.0fdb7f52.chunk.js"
+  },
+  {
+    "revision": "e26de4f0e0d98f1626b38316e0457409",
+    "url": "/subbythonleaderboard/static/js/2.0fdb7f52.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "088072d77defe50a8370",
+    "url": "/subbythonleaderboard/static/js/3.42b890a0.chunk.js"
+  },
+  {
+    "revision": "341cb40226484970b7db",
+    "url": "/subbythonleaderboard/static/js/main.fda4b0c3.chunk.js"
+  },
+  {
+    "revision": "30be8cf881480ca36cc2",
+    "url": "/subbythonleaderboard/static/js/runtime-main.bba3314b.js"
+  },
+  {
+    "revision": "b73ed447db8563e86fad0a82c4391323",
+    "url": "/subbythonleaderboard/static/media/eunaSanta.b73ed447.png"
+  },
+  {
+    "revision": "f54aaf4ffe339cacc8562e2b1ede952f",
+    "url": "/subbythonleaderboard/static/media/woof.f54aaf4f.png"
+  }
+]);
